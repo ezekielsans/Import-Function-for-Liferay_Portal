d222ec52228c67f8389d4101165402d68e595491
Ezekiel Santos <128892368+ezekielsans@users.noreply.github.com>
2023-11-24 19:12:02 +0800

    Update FileUploadController.java

--- FileUploadController.java
+++ FileUploadController.java
@@ -14,6 +14,7 @@ import javax.faces.bean.RequestScoped;
 import org.primefaces.event.FileUploadEvent;
 import org.primefaces.model.UploadedFile;
 import java.io.*;
+import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
@@ -106,100 +107,92 @@ public class FileUploadController implements Serializable {
     public void upload(FileUploadEvent event) throws IOException {
 
         UploadedFile uploadedFile = event.getFile();
+        System.out.println("UPLOADED FILE " + uploadedFile.getFileName());
 
-        System.out.println("UPLOADED FILE " + uploadedFile);
-        readExcel((DefaultUploadedFile) uploadedFile);
+        try (InputStream fileContent = uploadedFile.getInputstream()) {
 
-    }
-
-    public void readExcel(DefaultUploadedFile uploadedFileName) {
-        try {
-            // Specify the directory where uploaded files are stored
-            //String uploadDirectory = "/path/to/uploaded/files/";
-
-            // Construct the full path
-            //get the path of the project
-            // String projectDirectory = System.getProperty(uploadedFileName);
-           
-            // System.out.println("PROJECT DIRECTORY " + projectDirectory);
-            
-// projectDirectory + uploadedFileName;
-            //Method 1 start
-            //String filePath = "./fileUPload/sample_2.xlsx";
-            //XSSFWorkbook workbook = new XSSFWorkbook(new FileInputStream(filePath));
-//try this method #2
-            InputStream fileContent = uploadedFileName.getInputstream();
-
+            System.out.println("May laman " + fileContent);
             XSSFWorkbook workbook = new XSSFWorkbook(fileContent);
-
-            //to get the sheet
-            //getting the sheet through index cause idk the name of the tab
-            //METHOD 1
-//            XSSFSheet sheet = workbook.getSheetAt(0);
-//            int rowCount = sheet.getPhysicalNumberOfRows();
-//            System.out.println("No of rows: " + rowCount);
-//
-//            XSSFRow row = sheet.getRow(0);
-            XSSFSheet sheet = workbook.getSheetAt(0);
-            int i = 0;
-            XSSFRow row;
-            while ((row = sheet.getRow(i)) != null) {
-                System.out.println("Content of file " + row.getCell(0).getStringCellValue());
-                System.out.println("Content of file " + row.getCell(1).getStringCellValue());
-                System.out.println("Content of file " + row.getCell(2).getStringCellValue());
-                System.out.println("Content of file " + row.getCell(3).getStringCellValue());
-                System.out.println("Content of file " + row.getCell(4).getStringCellValue());
-                i++;
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-
-            System.out.println("May error ka ");
-        }
-    }
-
-    public void init() {
-        if (FacesContext.getCurrentInstance().isPostback() == false) {
-
-        }
-        initExcel();
-    }
-
-    public void initExcel() {
-        try {
-
-            System.out.println("pumasok dito");
-            // String projectDirectory = System.getProperty(uploadedFileName);
-            String fileName = "sample_2.xlsx";
-            String filePath = "/home/mis/Projects/Projects/zoom101/web/fileUpload";
-
-            File file = new File(filePath, fileName);
-            System.out.println("May laman " + file);
-            XSSFWorkbook workbook = new XSSFWorkbook(new FileInputStream(file));
 
             XSSFSheet sheet = workbook.getSheetAt(0);
 
             int rowCount = sheet.getPhysicalNumberOfRows();
 
             System.out.println("No of rows: " + rowCount);
-
-            XSSFRow row = sheet.getRow(0);
+//= sheet.getRow(0);
             int i = 0;
-
+            XSSFRow row;
             while ((row = sheet.getRow(i)) != null) {
-                System.out.println("This is the first row of file " + row.getCell(0).getStringCellValue() + "This is the 2nd row" + row.getCell(1).getStringCellValue() + "Content of the 3rd row" + row.getCell(2).getStringCellValue() + "Content of the 4th row" + row.getCell(3).getStringCellValue() + "Content of the 5th row" + row.getCell(4).getStringCellValue());
 
+                getFileUploadData().getUploadedList().add(new Object[]{
+                    row.getCell(0).getStringCellValue(),
+                    row.getCell(1).getStringCellValue(),
+                    row.getCell(2).getStringCellValue(),
+                    row.getCell(3).getStringCellValue(),
+                    row.getCell(4).getStringCellValue()
+                });
+                System.out.println("Content of file " + row.getCell(0).getStringCellValue());
                 System.out.println("Content of file " + row.getCell(1).getStringCellValue());
                 System.out.println("Content of file " + row.getCell(2).getStringCellValue());
                 System.out.println("Content of file " + row.getCell(3).getStringCellValue());
                 System.out.println("Content of file " + row.getCell(4).getStringCellValue());
                 i++;
+
             }
         } catch (IOException e) {
             e.printStackTrace();
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error", "Failed to process the uploaded file."));
 
-            System.out.println("May error ka ");
         }
+
     }
+
+    public void init() {
+        if (FacesContext.getCurrentInstance().isPostback() == false) {
+
+        }
+        // initExcel();
+    }
+    /*
+    
+     WORKING STATIC FILE READING
+
+     public void initExcel() {
+     try {
+
+     System.out.println("pumasok dito");
+     // String projectDirectory = System.getProperty(uploadedFileName);
+     String fileName = "sample_2.xlsx";
+     String filePath = "/home/mis/Projects/Projects/zoom101/web/fileUpload";
+
+     File file = new File(filePath, fileName);
+     System.out.println("May laman " + file);
+     XSSFWorkbook workbook = new XSSFWorkbook(new FileInputStream(file));
+
+     XSSFSheet sheet = workbook.getSheetAt(0);
+
+     int rowCount = sheet.getPhysicalNumberOfRows();
+
+     System.out.println("No of rows: " + rowCount);
+
+     XSSFRow row = sheet.getRow(0);
+     int i = 0;
+
+     while ((row = sheet.getRow(i)) != null) {
+     System.out.println("This is the first row of file " + row.getCell(0).getStringCellValue() + "This is the 2nd row" + row.getCell(1).getStringCellValue() + "Content of the 3rd row" + row.getCell(2).getStringCellValue() + "Content of the 4th row" + row.getCell(3).getStringCellValue() + "Content of the 5th row" + row.getCell(4).getStringCellValue());
+
+     System.out.println("Content of file " + row.getCell(1).getStringCellValue());
+     System.out.println("Content of file " + row.getCell(2).getStringCellValue());
+     System.out.println("Content of file " + row.getCell(3).getStringCellValue());
+     System.out.println("Content of file " + row.getCell(4).getStringCellValue());
+     i++;
+     }
+     } catch (IOException e) {
+     e.printStackTrace();
+
+     System.out.println("May error ka ");
+     }
+     }
+     */
 
 }
